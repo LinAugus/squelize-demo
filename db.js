f41576d28c0c03a6bbd226ea3690d33dbc6735e7
allin <aiceshine@163.com>
2017-09-20 17:40:36 +0800

    initial

---
+++ db.js
@@ -0,0 +1,45 @@
+const Sequelize = require('sequelize');
+
+
+// 连接数据库
+const db = new Sequelize(
+    'demo',
+    'dev',
+    'dev',
+    {
+        dialet: 'mysql',
+        host: 'localhost',
+        pool: {
+            max: 5,
+            min: 0,
+            idle: 1000
+        }
+    }
+);
+
+var User = db.define('user', {
+    username: {
+        type: Sequelize.STRING,
+        field: 'username'
+    },
+    nickname: {
+        type: Sequelize.STRING
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+}, {
+    freezeTableName: true
+});
+
+console.log(db.models);
+
+module.exports = User;
+
+// User.sync({ force: true }).then(() => {
+//     return User.create({
+//         username: 'Jon',
+//         nickname: 'lin',
+//         email: 'aa'
+//     });
+// });
